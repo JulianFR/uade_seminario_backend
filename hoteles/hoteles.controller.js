7a39ac5e79d4e395578d41e2f35f087969179582
unknown <joct89@gmail.com>
2020-06-09 13:07:20 -0300

    +hoteles

---
+++ hoteles/hoteles.controller.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HotelesController = void 0;
+var express_1 = __importDefault(require("express"));
+var main_util_1 = require("../main.util");
+var hotel_service_1 = require("./hotel.service");
+var HotelesController = /** @class */ (function () {
+    function HotelesController() {
+        this.ruta = express_1.default.Router();
+        this.ruta
+            .get("/hotel/:id", main_util_1.atraparErroresAsincronos(hotel_service_1.getHotel))
+            .post("/hoteles", main_util_1.atraparErroresAsincronos(hotel_service_1.postHotel))
+            .put("/hoteles", main_util_1.atraparErroresAsincronos(hotel_service_1.putHotel));
+    }
+    return HotelesController;
+}());
+exports.HotelesController = HotelesController;
--- main.js
+++ main.js
@@ -8,11 +8,13 @@ var morgan_1 = __importDefault(require("morgan"));
 var main_middleware_1 = require("./main.middleware");
 var credenciales_controller_1 = require("./credenciales/credenciales.controller");
 var usuario_controller_1 = require("./usuario/usuario.controller");
+var hoteles_controller_1 = require("./hoteles/hoteles.controller");
 express_1.default()
     .use(morgan_1.default("dev"))
     .use(express_1.default.json())
     .use(main_middleware_1.cors)
     .use('/api/v1.0/', new credenciales_controller_1.CredencialesController().rutador)
     .use('/api/v1.0/', new usuario_controller_1.UsuarioController().rutador)
+    .use('/api/v1.0/', new hoteles_controller_1.HotelesController().ruta)
     .use(main_middleware_1.manejadorDeErrores)
     .listen(process.env.PORT || 3000);
